64d3282b963547e1b7cd20ac3c6d8641a8da1e06
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-01-09 16:22:26 +0200

    Created Proposal component with data model

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,8 +16,9 @@ import { AppRoutingModule } from './app-routing.module';
 	],
 	declarations: [
 		AppComponent,
-		HomepageComponent
-		DocumentsComponent
+		HomepageComponent,
+		DocumentsComponent,
+		ProposalListComponent
 	],
 	bootstrap: [
 		AppComponent
